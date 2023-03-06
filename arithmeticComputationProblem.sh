862bfecdbb4d89a7fe443e9199da6f3a43d6a844
shephali09 <hiremathshephali9@gmail.com>
2023-03-06 15:35:57 +0530

    Add: Store the results in a Dictionary for every computation

--- arithmeticComputationProblem.sh
+++ arithmeticComputationProblem.sh
@@ -2,5 +2,19 @@ read -p "Enter a:" a
 read -p "Enter b:" b
 read -p "Enter c:" c
 
-arithmeticOperation3=$(echo $a $b $c | awk '{print $1%$2+$3}')
+temp=$(echo $b $c | awk '{print $1*$2}')
+arithmeticOperation1=$(echo $a $temp | awk '{print $1+$2}')
+
+arithmeticOperation2=$(echo $a $b $c | awk '{print $1*$2+$3}')
+
+temp=$(echo $a $b| awk '{print $1/$2}')
+arithmeticOperation3=$(echo $temp $c | awk '{print $1+$2}')
+
+arithmeticOperation4=$(echo $a $b $c | awk '{print $1%$2+$3}')
+
+declare -A results
+results["Result1"]=$arithmeticOperation1
+results["Result2"]=$arithmeticOperation2
+results["Result3"]=$arithmeticOperation3
+results["Result4"]=$arithmeticOperation4
 
