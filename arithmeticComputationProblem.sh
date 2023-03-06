71513e697bad58673c8e28ab720f31c404c6a1cd
shephali09 <hiremathshephali9@gmail.com>
2023-03-06 12:49:57 +0530

    Add: Compute a % b + c

--- arithmeticComputationProblem.sh
+++ arithmeticComputationProblem.sh
@@ -2,6 +2,5 @@ read -p "Enter a:" a
 read -p "Enter b:" b
 read -p "Enter c:" c
 
-temp=$(echo $a $b| awk '{print $1/$2}')
-arithmeticOperation2=$(echo $temp $c | awk '{print $1+$2}')
+arithmeticOperation3=$(echo $a $b $c | awk '{print $1%$2+$3}')
 
