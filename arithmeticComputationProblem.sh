ec280ae4b5da8fa8b590c04a661605cf6810b224
shephali09 <hiremathshephali9@gmail.com>
2023-03-06 22:58:46 +0530

    Add: Read the values from the Dictionary into the array

--- arithmeticComputationProblem.sh
+++ arithmeticComputationProblem.sh
@@ -12,9 +12,14 @@ arithmeticOperation3=$(echo $temp $c | awk '{print $1+$2}')
 
 arithmeticOperation4=$(echo $a $b $c | awk '{print $1%$2+$3}')
 
-declare -A results
-results["Result1"]=$arithmeticOperation1
-results["Result2"]=$arithmeticOperation2
-results["Result3"]=$arithmeticOperation3
-results["Result4"]=$arithmeticOperation4
+declare -A results 
+results["Result1"]=$arithmeticOperation1 
+results["Result2"]=$arithmeticOperation2 
+results["Result3"]=$arithmeticOperation3 
+results["Result4"]=$arithmeticOperation4 
 
+array=()
+for key in "${!results[@]}"
+do
+  array+=("${results[$key]}")
+done
